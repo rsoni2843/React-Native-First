b13f29b11d2788c0be14be36b000a1bd5d02181c
Nipendra Soni <rsoni2843@gmail.com>
2023-01-16 15:17:55 +0530

    image component

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import CustomComponent from "./src/CustomComponent";
+import FlatListDemo from "./src/FlatListDemo";
+import Images from "./src/Images";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text style={styles.t1}>Hello User</Text>
+      {/* <FlatListDemo /> */}
+      <Images />
+      {/* <CustomComponent /> */}
       {/* <StatusBar style="auto" /> */}
     </View>
   );
@@ -12,9 +18,12 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    marginTop: 20,
+  },
+  t1: {
+    color: "red",
   },
 });
---
+++ src/CustomComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+
+function CustomComponent() {
+  return <Text>This is a custom component</Text>;
+}
+export default CustomComponent;
